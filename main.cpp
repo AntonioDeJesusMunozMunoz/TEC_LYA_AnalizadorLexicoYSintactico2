58df9d18315c544d00cc48a21d1e555709204d82
AJMM <allahuakbarr1230@gmail.com>
2025-12-16 22:18:35 -0600

    se pasaron los 10 tests intensivos, se corrigieron los errores que se presentaron

--- main.cpp
+++ main.cpp
@@ -87,57 +87,58 @@ char token[22][8] = {"x", ";", ",", "*", "Id", "[", "]", "Num", "char", "int", "
 
 char varsint[15][3]={"x", "D", "L", "L'", "I", "I'", "A", "A'", "K",
                        "T", "F", "E", "P"};
-    // e -> cadena vacia
-int tablaM[100][8]= {{1, 5, 1, 5, 999, -1, 999, 999}, //[ D->I';
-                                 {1, 5, 1, 5, 999, -1, 999, 999}, //; D->I';
+    // e -> cadena vacia         //X  A  ?
+int tablaM[100][8]= {             {1, 5, 1, 5, 999, -1, 999, 999}, //[ D->I';
+                                  {1, 5, 1, 5, 999, -1, 999, 999}, //; D->I';
                                   {1, 1, 1, 5, 999, -1, 999, 999}, //id D->PL';
                                   {1, 7, 1, 5, 3, -1, 999, 999}, //num D->I'L';
                                   {1, 8, 1, 9, 2, -1, 999, 999}, //char D->TL;
                                   {1, 9, 1, 9, 2, -1, 999, 999}, //int D->TL;
-    {1, 10, 1, 9, 2, -1, 999, 999}, //float D->TL;
-    /* 5 */      {2, 3, 2, 4, 3, 999, 999, 999}, // * L->IL'
-    {2, 4, 2, 4, 3, 999, 999, 999}, //Id L->IL'
-    {3, 1, 3, 5, 999, 999, 999, 999}, //; L'->I'
-    {3, 1, 3, 999, 999, 999, 999, 999},//; L'->e
-    {3, 2, 3, -2, 4, 3, 999, 999}, //, L'->,IL'
-    /* 10 */      {3, 3, 3, -3, 8, 3, 999, 999}, //* L'->*KL'
-    {3, 18, 3, 12, 3, 999, 999, 999}, //+ L'->PL'
-    {3, 19, 3, 12, 3, 999, 999, 999}, //- L'->PL'
-    {3, 20, 3, 12, 3, 999, 999, 999}, // / L'->PL'
-    {3, 14, 3, -14, 5, 999, 999, 999},// cte. Lit. L'->cte Lit. I'
-    {4, 3, 4, -3, -4, 5, 999, 999}, // * I->*Id I'
-    {4, 4, 4, -4, 5, 999, 999, 999},// Id I->Id I'
-    {5, 1, 5, 999, 999, 999, 999, 999},// ; I'->e
-    /* 15 */       {5, 2, 5, 999, 999, 999, 999, 999},// , I'->e
-    {5, 5, 5, 6, 999, 999, 999, 999}, // [ I' -> A
-    //{5, 7, 5, -7, 999, 999, 999, 999}, //Num I' -> Num
-    //{5, 12, 5, 7, 8, 11, 999, 999}, //( I' -> Num
-    {5, 14, 5, -14, 999, 999, 999, 999}, //cte lit I'->cte lit
-    /* -> */       {5, 17, 5, -17, 8, 999, 999, 999}, //= I'->=K
-    {6, 5, 6, -5, 8, 11, 999, 999}, //[ A->[ KE
-    /* 20 */       {7, 1, 7, 999, 999, 999, 999, 999},//; A'->e
-    {7, 2, 7, 999, 999, 999, 999, 999},//, A'->e
-    {7, 5, 7, -5, 8, -6, 7, 999}, //[ A' -> [K]A'
-    {7, 12, 7, -12, 999, 999, 999, 999}, //( A' -> (
-    {8, 4, 8, -4, 999, 999, 999, 999}, //Id K -> Id
-    {8, 5, 8, -5, 8, 11, 999, 999}, //[ K -> [ KE
-    {8, 7, 8, -7, 999, 999, 999, 999}, //Num K -> Num
-    {8, 21, 8, -21, 999, 999, 999, 999}, //Real K -> Num
-    {8, 12, 8, -12, 8, 11, 999, 999}, //( K -> ( KE
-    {8, 15, 8, -15, 8, 10, 999, 999}, //{ K -> { KF
-    {9, 8, 9, -8, 999, 999, 999, 999}, //char T->Char
-    {9, 9, 9, -9, 999, 999, 999, 999}, //int T->int
-    {9, 10, 9, -10, 999, 999, 999, 999},//float T->float
-    /*segunda gramatica */
-    /* 25 */    {10, 11, 10, -11, 11, -1, 999, 999},//puts F-> puts E;
-    {10, 16, 10, -16, 999, 999, 999, 999},//} F-> };
-    {11, 6, 11, -6, 1, 999, 999, 999},//]E -> ]D
-    {11, 12, 11, -12, 12, -13, 999, 999},//( E -> (P)
-    {11, 13, 11, -13, 999, 999, 999, 999},//)E -> )
-    {12, 14, 12, -14, 999, 999, 999, 999},//cte lit P->cte lit
-    {12, 18, 12, -18, 8, 999, 999, 999},//+ P -> + k
-    {12, 19, 12, -19, 8, 999, 999, 999},//- P -> - k
-    {12, 20, 12, -20, 8, 999, 999, 999},// / P ->/ k
+                                  {1, 10, 1, 9, 2, -1, 999, 999}, //float D->TL;
+                     /* 5 */      {2, 3, 2, 4, 3, 999, 999, 999}, // * L->IL'
+                                  {2, 4, 2, 4, 3, 999, 999, 999}, //Id L->IL'
+                                  {3, 1, 3, 5, 999, 999, 999, 999}, //; L'->I'
+                                  {3, 1, 3, 999, 999, 999, 999, 999},//; L'->e
+                                  {3, 2, 3, -2, 4, 3, 999, 999}, //, L'->,IL'
+                    /* 10 */      {3, 3, 3, -3, 8, 3, 999, 999}, //* L'->*KL'
+                                  {3, 18, 3, 12, 3, 999, 999, 999}, //+ L'->PL'
+                                  {3, 19, 3, 12, 3, 999, 999, 999}, //- L'->PL'
+                                  {3, 20, 3, 12, 3, 999, 999, 999}, // / L'->PL'
+                                  {3, 14, 3, -14, 5, 999, 999, 999},// cte. Lit. L'->cte Lit. I'
+                                  {4, 3, 4, -3, -4, 5, 999, 999}, // * I->*Id I'
+                                  {4, 4, 4, -4, 5, 999, 999, 999},// Id I->Id I'
+                                  {5, 1, 5, 999, 999, 999, 999, 999},// ; I'->e
+                   /* 15 */       {5, 2, 5, 999, 999, 999, 999, 999},// , I'->e
+                                  {5, 5, 5, 6, 999, 999, 999, 999}, // [ I' -> A
+                                //{5, 7, 5, -7, 999, 999, 999, 999}, //Num I' -> Num
+                                //{5, 12, 5, 7, 8, 11, 999, 999}, //( I' -> Num
+                                  {5, 14, 5, -14, 999, 999, 999, 999}, //cte lit I'->cte lit
+                   /* -> */       {5, 17, 5, -17, 8, 999, 999, 999}, //= I'->=K
+                                  {6, 5, 6, -5, 8, 11, 999, 999}, //[ A->[ KE
+                   /* 20 */       {7, 1, 7, 999, 999, 999, 999, 999},//; A'->e
+                                  {7, 2, 7, 999, 999, 999, 999, 999},//, A'->e
+                                  {7, 5, 7, -5, 8, -6, 7, 999}, //[ A' -> [K]A'
+                                  {7, 12, 7, -12, 999, 999, 999, 999}, //( A' -> (
+                                  {8, 4, 8, -4, 999, 999, 999, 999}, //Id K -> Id
+                                  {8, 5, 8, -5, 8, 11, 999, 999}, //[ K -> [ KE
+                                  {8, 7, 8, -7, 999, 999, 999, 999}, //Num K -> Num
+                                  {8, 21, 8, -21, 999, 999, 999, 999}, //Real K -> Num
+                                  {8, 12, 8, -12, 8, 11, 999, 999}, //( K -> ( KE
+                                  {8, 15, 8, -15, 8, 10, 999, 999}, //{ K -> { KF
+                                  {9, 8, 9, -8, 999, 999, 999, 999}, //char T->Char
+                                  {9, 9, 9, -9, 999, 999, 999, 999}, //int T->int
+                                  {9, 10, 9, -10, 999, 999, 999, 999},//float T->float
+                                  /*segunda gramatica */
+                      /* 25 */    {10, 11, 10, -11, 11, -1, 999, 999},//puts F-> puts E;
+                                  {10, 16, 10, -16, 999, 999, 999, 999},//} F-> };
+                                  {11, 6, 11, -6, 1, 999, 999, 999},//]E -> ]D
+                                  {11, 12, 11, -12, 12, -13, 999, 999},//( E -> (P)
+                                  {11, 13, 11, -13, 999, 999, 999, 999},//)E -> )
+                                  {11, 18, 11, 12,  11, 999, 999, 999},//+E -> PE (ESTA SE LA PUSE YO TONIO
+                                  {12, 14, 12, -14, 999, 999, 999, 999},//cte lit P->cte lit
+                                  {12, 18, 12, -18, 8, 999, 999, 999},//+ P -> + k
+                                  {12, 19, 12, -19, 8, 999, 999, 999},//- P -> - k
+                                  {12, 20, 12, -20, 8, 999, 999, 999},// / P ->/ k  
 };
 
 
@@ -230,8 +231,8 @@ void generararch()
         car = cin.get();
 
         //si es \n, salto linea
-        if( car == '\n')
-            printf("\n");
+        //if( car == '\n')
+        //    printf("\n");
 
         //si no es @ o BACKSPACE, lo escribe
         if(car != '@' && car != 8)
@@ -791,42 +792,53 @@ void vmuestra()
 
 void vanalisis_sintactico()
 {
+    /*LAS VARIABLES QUE VOY A USAR*/
     int ip=0, i, j;
     int renglon, iast;
-    char x[10], a[10];
+    char x[10], a[10];  //las que se usaran para la tablaM
 
+    //la pila inicia con un $ como en la teoria
     insertapila("$");
 
+    //si la primer token es puts, se usa la gramatica extendida (iniciar con F en vez de D)
     if(strcmp(asTokens[ip], "puts") == 0)
         insertapila("F");
     else
         insertapila("D");
 
+    /*IMPRIMIMOS TODOS LOS TOKEN*/
     printf("\nSalida del Analizador Sintactico (asTokens): \n");
     printf("Arreglo de Tokens: \n");
 
     for(i=0; strcmp(asTokens[i], "$") != 0; i++)
         printf("%s ", asTokens[i]);
 
+    /*CONSEGUIMOS E IMPRIMIMOS LAS PRODUCCIONES*/
     printf("\n\n Producciones: \n");
 
     do
     {
+        //consigo el primer caracter en la pila y en a
         strcpy(x, pilac[cima]);
         strcpy(a, asTokens[ip]);
 
+        //si es terminal o $
         if(estoken(x) || (strcmp(x, "$") == 0))
         {
+            //si son iguales, los elimino
             if(strcmp(x, a) == 0)
             {
                 eliminapila();
                 ip++;
             }
+
+            //si no
             else
             {
+                //vuelvo a añadir el simbolo inicial?
                 if(strcmp(asTokens[ip], "puts") == 0)
                     insertapila("F");
-                else //Sino
+                else
                     insertapila("D");
 
                 strcpy(x, pilac[cima]);
@@ -835,23 +847,34 @@ void vanalisis_sintactico()
         }
         else
         {
+            //consigo el renglón donde esta la prod
             renglon = buscaTabla(a, x);
 
+            //si no es vacio
             if(renglon != 999)
             {
+                //elimino el token de la pila
                 eliminapila();
                 iast = 0;
+
+                /*PRINTEAR PROD*/
+                //printeo el lado izquierdo de la prod (siempre en j = 0)
                 printf("%-3s -> ", varsint[tablaM[renglon][0]]);
 
+                //recorro ese renglón de la tabla M, saltando los espacios que uso para otras cosas
                 for(j= 3; iast!=999; j++)
                 {
-                    iast = tablaM[renglon][j];  //999
+
+                    //consigo la i del caracter que voy a imprimir
+                    iast = tablaM[renglon][j];
+                    //si es negativo, es terminal
                     if(iast < 0)
                     {
                         iast *= -1;
                         printf("%s ",token[iast]);
 
                     }
+                    //si es positivo, es no terminal
                     else
                     {
                         if(iast != 999)
@@ -859,20 +882,27 @@ void vanalisis_sintactico()
                     }
                 }
 
+                //salto linea
                 printf("\n");
 
+                /*METER LA PROD A LA PILA*/
                 for(i=j-2; i>2; i--)
                 {
+                    //si es negativo, inserto un token terminal
                     iast = tablaM[renglon][i];
                     if(iast < 0)
                     {
                         iast *= -1;
                         insertapila(token[iast]);
                     }
+
+                    //si no, inserto el token terminal
                     else
                         insertapila(varsint[iast]);
                 }
             }
+
+            //si si es vacio, es sintaxis invalida
             else
             {
                 printf("\n\n Error de Sintaxis");
@@ -880,7 +910,7 @@ void vanalisis_sintactico()
             }
         }
     }while(strcmp(x, "$") != 0);
-    printf("\n");
+    printf("\nSintaxis valida\n");
 }
 
 void insertapila(string elem)//(char *elem) //(char elem[])
@@ -926,21 +956,26 @@ int buscaTabla(char a[], char x[])
 {
     int indx=0, inda=0, i;
 
+    /*CONSIGO CUAL INDICE CORRESPONDE A EL TOKEN EN A*/
     for(i=0; i<23; i++)
         if(strcmp(a, token[i]) == 0)
             inda = i;//9 int
 
+    /*CONSIGO CUAL INDICE CORRESPONDE A EL TOKEN EN X*/
     for(i=0; i<15; i++)
         if(strcmp(x, varsint[i]) == 0)
             indx=i; //1 D
 
-    for(i=0; i<44; i++)
+    /*RECORRO LOS PRIMEROS 44 RENGLONES DE LA TABLA PORQUE SOLO USO ESOS*/
+    for(i=0; i<50; i++)
     {
+        /*SI EL INDX Y EL INDA CONCUERDAN CON LOS VALORES EN ALGUN RENGLON, ESE RENGLON TIENE LA PROD*/
         if(indx == tablaM[i][0])
             if(inda == tablaM[i][1])
-                return i;
+                return i; /*REGRESO EL INDICE DEL RENGLON DND ESTA LA PROD*/
     }
 
+    /*SI NO COINCIDE, REGRESO 999 QUE SE ENTIENDE COMO ERROR*/
     return 999;
 }
 
